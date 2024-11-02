f14f5bd5b2e45c8e58481f207bf4c3368e0ada5e
MuhammadAhsaanAbbasi <mahsaanabbasi@gmail.com>
2024-11-02 22:22:09 +0500

    StartupDetails & views are completed

--- src/app/(root)/startup/[id]/page.tsx
+++ src/app/(root)/startup/[id]/page.tsx
@@ -1,6 +1,7 @@
 import StartUpDetails from '@/components/startup/StartUpDetails';
 import Views from '@/components/startup/Views';
-import { sanityFetch } from '@/sanity/lib/live';
+import { client } from '@/sanity/lib/client';
+// import { sanityFetch } from '@/sanity/lib/live';
 import { STARTUP_BY_ID_QUERY } from '@/sanity/lib/queries';
 import { notFound } from 'next/navigation';
 import React, { Suspense } from 'react'
@@ -12,10 +13,10 @@ interface params {
 }
 
 const StartUpPage = async ({ params }: params) => {
-    const Id = (await params).id;
-    const startUpId = { id: Id || null };
+    const id = (await params).id;
+    // const startUpId = { id: Id || null };
 
-    const { data: post } = await sanityFetch({ query: STARTUP_BY_ID_QUERY, params: startUpId });
+    const post = await client.fetch(STARTUP_BY_ID_QUERY, { id });
 
     if (!post) return notFound();
 
@@ -23,7 +24,7 @@ const StartUpPage = async ({ params }: params) => {
         <main>
             <StartUpDetails startDetails={post} />
             <Suspense fallback={<div>Loading...</div>}>
-                <Views id={Id} />
+                <Views id={id} />
             </Suspense>
         </main>
     )
