4876dc5bc3d2a8108f3a2dc0561b6d29f644388f
MuhammadAhsaanAbbasi <mahsaanabbasi@gmail.com>
2024-11-03 15:24:15 +0500

    StartupForm & partial pre-rendring

--- src/sanity/lib/queries.ts
+++ src/sanity/lib/queries.ts
@@ -36,7 +36,7 @@ export const STARTUP_BY_ID_QUERY = defineQuery(
 }`
 )
 
-export const STARTUP_Views_QUERY = defineQuery(
+export const STARTUP_VIEWS_QUERY = defineQuery(
     `*[_type == "startup" && _id == $id][0]{
     _id, 
     views,
