e76055b1163190424c22cd0b711302c69d4d0376
MuhammadAhsaanAbbasi <mahsaanabbasi@gmail.com>
2024-11-05 12:27:26 +0500

    SanityLive browser token enabled & server token changed

--- src/sanity/lib/live.ts
+++ src/sanity/lib/live.ts
@@ -1,9 +1,10 @@
+// live.ts
 import { defineLive } from "next-sanity";
 import "server-only";
 import { client } from "./client";
 
 export const { sanityFetch, SanityLive } = defineLive({
     client,
-    serverToken: process.env.SANITY_SERVER_TOKEN, // Add server token here
-    // browserToken: process.env.NEXT_PUBLIC_SANITY_BROWSER_TOKEN, // Add browser token here
+    serverToken: process.env.SANITY_SERVER_TOKEN, // Use without NEXT_PUBLIC_
+    browserToken: process.env.NEXT_PUBLIC_SANITY_BROWSER_TOKEN, // Keep NEXT_PUBLIC_
 });
